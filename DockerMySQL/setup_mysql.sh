b2d770f5409d262c3fbcef33ce1a93c8e5704ad0
Thomas S. Kokholm <ThomasSKokholm@gmail.com>
2020-07-03 15:05:40 +0200

    Opretter mysql docker med db opsætning.

---
+++ DockerMySQL/setup_mysql.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+git clone https://github.com/os2kitos/Softwareoversigten.git
+
+cd Softwareoversigten
+
+mkdir mysql
+cp installation/localkitoslookup.sql /mysql/setup.sql
+
+
